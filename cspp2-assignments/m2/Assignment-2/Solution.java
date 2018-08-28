42de7d1edd0152bc614b0a9f33fae7afbbac8b84
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-29 00:05:20 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -9,30 +9,30 @@ import java.lang.*;
  * Class for solution.
  */
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
+    /*
+    Do not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
 
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/* 
-		*
-	*/
-	static void rootsOfQuadraticEquation(final int a, final  int b, final int c) {
-		/**
-		 * roots
-		 */
-		int f = 4;
-		double det = Math.sqrt((b * b) - f * a * c);
-		double root1 = (-b + det) / 2 * a;
-		double root2 = (-b - det) / 2 * a;
-		System.out.print(root1 + " ");
-		System.out.print(root2);
-	}
+        rootsOfQuadE(a, b, c);
+    }
+    /* 
+        *
+    */
+    static void rootsOfQuadE(final int a, final  int b, final int c) {
+        /**
+         * roots
+         */
+        int f = 4;
+        double det = Math.sqrt((b * b) - f * a * c);
+        double root1 = (-b + det) / 2 * a;
+        double root2 = (-b - det) / 2 * a;
+        System.out.print(root1 + " ");
+        System.out.print(root2);
+    }
 }
