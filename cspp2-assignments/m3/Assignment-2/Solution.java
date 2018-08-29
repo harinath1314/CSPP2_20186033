43a99b9990d235eed20ff51fc6994d89d34ed164
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-29 13:33:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -1,42 +1,44 @@
 /**program to print number of 7's in given number to 1.
  *author harinath
  *date : 29/08/18
- * 
+ *
  */
 import java.util.Scanner;
 
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 /**
  * Class for solution.
  */
 public class Solution {
 /* Fill the main function to print the number of 7's between 1 to n*/
-	/**
-	 * { this is main method }
-	 *
-	 * @param      args  The arguments
-	 */
-    public static void main(String[] args) {
-    	/**
-    	 * { var_description }
-    	 */
+    /**.
+     * { this is main method }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int refer = 7;
+        final int refer = 7;
+        final int div = 10;
         int count = 0;
-        for (int i =7 ;i<=n;i++ ) {
-        	{
-        		int num =i;
-        		while (num>0) {
-        			int digit = num % 10;
-        			num=num/10;
-        			if (digit == refer) count+=1;
-        		}
-        	}
-        	
+        for (int i =7 ; i <= n; i ++ ) {
+            {
+                int num = i;
+                while (num > 0) {
+                    int digit = num % div;
+                    num = num / div;
+                    if (digit == refer){
+                        count += 1;}
+                }
+            }
+            
         }
         System.out.println(count);
     }
