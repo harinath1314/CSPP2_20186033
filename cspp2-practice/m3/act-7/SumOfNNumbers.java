8785b8db59e986e3439c3eb4433dda1472d98b7e
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-29 11:16:55 +0530

    act-7

---
+++ cspp2-practice/m3/act-7/SumOfNNumbers.java
@@ -0,0 +1,24 @@
+/**
+ * finding sumof N numbers.
+ * author harinath
+ * Date : 29/08/18
+ */
+import java.util.Scanner;
+public class SumOfNNumbers {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * { item_description }
+         */
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println((a*(a+1))/2);
+
+
+        
+    }
+}
