071c30892a2486221522dd4a12a60fc001d63d12
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-29 11:16:29 +0530

    act-6

---
+++ cspp2-practice/m3/act-6/ConverttoWhileLoop.java
@@ -0,0 +1,18 @@
+/**
+ * Class for convertto while loop.
+ */
+public class ConverttoWhileLoop {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		int l = 0;
+		while (l<10) {
+			l+=2;
+			System.out.println(l);
+		}
+		System.out.println("GoodBye");
+	}
+}
