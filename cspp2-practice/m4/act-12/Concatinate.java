0e367e3ec57fd7455e80fe72a6d939ce2a49e3a0
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-30 15:57:53 +0530

    act-12

---
+++ cspp2-practice/m4/act-12/Concatinate.java
@@ -0,0 +1,27 @@
+/**
+ * { item_description }
+ */
+
+import java.util.Scanner;
+/**
+ * Class for concatinate.
+ */
+
+public final class Concatinate {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	private Concatinate() {
+		/**
+		 * { item_description }
+		 */
+	}
+	public static void main(final String[] args) {
+		Scanner input = new Scanner(System.in);
+		String inp = input.nextLine();
+		System.out.print("Hello " + inp + "!");
+		
+	}
+}
