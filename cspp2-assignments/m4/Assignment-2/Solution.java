77982dfd893fdd1857d40dbc20f0e9250ee95b55
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-30 15:20:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,7 +1,9 @@
 /**.
  * { we are importing packages of java}
  */
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+
 /**.
  	* Class for solution.
  	* author harinath reddy
@@ -15,17 +17,23 @@ public final class Solution {
 	 */
 	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
-		int m = input.nextInt();
-		int n = input.nextInt();
-		int o = input.nextInt();
-		int p = input.nextInt();
+		String inp = input.nextLine();
+		System.out.println(inp);
+		String[] input_array = inp.split(" ");
+		System.out.println(Arrays.toString(input_array));
+ 		int m = Integer.parseInt(input_array[0]);
+		int n = Integer.parseInt(input_array[1]);
 		int[][] mat1 = new int[m][n];
-		int[][] mat2 = new int[o][p];
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n ; j++ ) {
 				mat1[i][j] = input.nextInt();
 			}
 		}
+		String input2 = input.nextLine();
+		String[] input_array2 = input2.split(" ");
+		int o = Integer.parseInt(input_array2[0]);
+		int p = Integer.parseInt(input_array2[1]);
+		int[][] mat2 = new int[o][p];
 		for (int i = 0; i < o; i++) {
 			for (int j = 0; j < p; j++ ) {
 				mat2[i][j] = input.nextInt();
@@ -38,6 +46,6 @@ public final class Solution {
 				}
 			}
 		} else
-			System.out.println("addition is not possible");
+			System.out.println("not possible");
 	}
 }
