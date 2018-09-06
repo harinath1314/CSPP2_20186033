b8fb9c8ebfc36a6f13ad4fc5a8a7b3fd8c905b20
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-06 15:11:49 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/List.java
+++ cspp2-assignments/m10/Assignment-3/List.java
@@ -87,7 +87,7 @@ public class List<E> {
     public void remove(int index) {
         //Write logic for remove method
         if ( index >= 0 && index <= size) {
-        for (int i = 0; i < size; i++) {
+        for (int i = index; i < size; i++) {
             list[i] = list[i + 1];
         }
         size -= 1;
