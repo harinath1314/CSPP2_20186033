b84eb2962ad1b43513b2c09bd76d99b63b7d24a5
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-11 17:38:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -2,9 +2,9 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 /**
- * Class for set.
+ * Class for SortedSetADT.
  * @author : harinatha reddy
- * date 
+ * date
  */
 class SortedSetADT {
     /**
