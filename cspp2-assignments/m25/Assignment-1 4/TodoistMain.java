b74923bb3aeb8cb48443ee0ea8a54ca3eabc936a
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-22 11:43:26 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1 4/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1 4/TodoistMain.java
@@ -169,11 +169,9 @@ public class TodoistMain {
      */
     public static Task createTask(final String[] tokens) throws Exception {
         String title = tokens[1];
-        System.out.println(tokens[1]);
-        if(tokens[1] == ""){
+        if(title == ""){
         	throw new Exception("Title not provide");
         }
-        System.out.println("hari how are you");
         String assignedTo = tokens[2];
         int timeToComplete = Integer.parseInt(tokens[3]);
         boolean important = tokens[4].equals("y");
