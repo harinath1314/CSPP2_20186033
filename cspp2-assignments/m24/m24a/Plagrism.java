943f865ff1a042e4a88acd87bfd86f26f7439b67
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-23 14:36:03 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/m24a/Plagrism.java
+++ cspp2-assignments/m24/m24a/Plagrism.java
@@ -47,7 +47,7 @@ class Plagrism {
 
                     String lines;
                     while ((lines = buff.readLine()) != null ) {
-                        String[] line = lines.replaceAll("[^a-zA-Z_0-9 ]", "").replaceAll("\n"," ").toLowerCase().split(" ");
+                        String[] line = lines.replaceAll("[^a-zA-Z_0-9. ]", "").replaceAll("\n"," ").toLowerCase().split(" ");
                         // System.out.println(Arrays.toString(line));
                         // String[] line = lines.toLowerCase().split(" ");
                         // if (line[0] != "\n") {
@@ -72,7 +72,7 @@ class Plagrism {
                     }
 
                     while ((lines = buff2.readLine()) != null ) {
-                        String [] line = lines.replaceAll("[^a-zA-Z_0-9 ]", "").replaceAll("\n"," ").toLowerCase().split(" ");
+                        String [] line = lines.replaceAll("[^a-zA-Z_0-9. ]", "").replaceAll("\n"," ").toLowerCase().split(" ");
 
 
                         // String [] line = lines.toLowerCase().split(" ");
